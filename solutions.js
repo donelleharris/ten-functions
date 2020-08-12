5b9b819d27833542773c3bcd1a9f0f8159692639
Daniel Fryar <daniel.fryar@gmail.com>
2020-08-12 11:12:07 -0500

    finished isTrue

--- solutions.js
+++ solutions.js
@@ -6,7 +6,7 @@
  * **Note**: While normally it is good practice to wrap your javascript in an
  * immediately invoked function expression (iife), you should _not_ do that
  * here. If you do, the automated tests will not be able to see your functions.
- * If you don't know what an iffe is, don't worry about it :)
+ * If you don't know what an iife is, don't worry about it :)
  *
  * This file is for defining the following functions. See README.md for more detail.
  * isTrue
@@ -20,3 +20,7 @@
  * and
  * concat
  */
+
+function isTrue(input) {
+    return input === true;
+}
